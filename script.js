8c7381b9312777557e6e2f5115d082bf0fdc0e89
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-10 20:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 //complete this code
 class Rectangle {
-	constructor(width,hieght){
-		this.width=width;
-		this.height=hieght;
-	}
-	getArea(){
-		return this.width*this.hieght;
-	}
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this._width * this._height;
+    }
 }
 
 class Square extends Rectangle {
-	getPerimeter(){
-		return 2(this.width+this.hieght);
-	}
+    constructor(side) {
+        // Call the constructor of the parent class (Rectangle)
+        super(side, side);
+}
+
+    getPerimeter() {
+        // Perimeter of a square is 4 times the length of one side
+        return 4 * this.width;
+    }
 }
 // let rect=new Rectangle(2,3);
 // Do not change the code below this line
